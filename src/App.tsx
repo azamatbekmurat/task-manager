e70587df3b120f91a66776a7ffd573fb78975e7e
Azamat <azaege@gmail.com>
2025-04-24 01:39:35 -0700

    Add Dark Mode Toggle

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,31 @@ import React from 'react';
 import TaskInput from './components/TaskInput';
 import TaskList from './features/tasks/TaskList';
 import './styles.css';
+import { ThemeProvider, ThemeContext } from "./ThemeContext";
 
-function App() {
+const App = () => {
   return (
-    <div className="app">
-      <h1>Task Manager App</h1>
-      <TaskInput />
-      <TaskList />
+    <ThemeProvider>
+      <Main />
+    </ThemeProvider>
+  );
+};
+
+const Main = () => {
+  const { isDark, toggleTheme } = React.useContext(ThemeContext);
+
+  return (
+    <div className={`app ${isDark ? "dark" : ""}`}>
+      <button onClick={toggleTheme}>
+        Switch to {isDark ? "Light" : "Dark"} Mode
+      </button>
+      <div className="app">
+        <h1>Task Manager App</h1>
+        <TaskInput />
+        <TaskList />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
